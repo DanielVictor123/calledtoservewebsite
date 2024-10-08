0559f37bf621063b0cdcf45b946a625a6bd20806
DanielVictor123 <isaacvictor942006@gmail.com>
2024-10-08 23:38:44 +0100

    Full Carousel Updates

--- agege-stake/scripts/test-users.js
+++ agege-stake/scripts/test-users.js
@@ -25,7 +25,7 @@ fetch("missionaries.json")
       missionaryCard.classList.add("col-12", "col-sm-6", "col-md-4");
 
       missionaryCard.innerHTML = `
-        <div class="card container mt-5">
+        <div class="card col-lg-12 mt-5">
           <img src="${missionary.image}" loading="lazy" alt="Profile picture" class="card-img-top">
           <div class="card-body">
             <h4 class="card-title text-primary">${missionary.name}</h4>
@@ -65,42 +65,3 @@ fetch("missionaries.json")
   .catch((error) => {
     console.error("Error fetching missionaries:", error);
   });
-
-// fetch("missionaries.json")
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     const missionariesList = document.getElementById("missionariesList");
-
-//     // Clear existing content if any
-//     missionariesList.innerHTML = "";
-
-//     // Loop through each missionary and create HTML
-//     data.forEach((missionary) => {
-//       const missionaryCard = document.createElement("div");
-//       missionaryCard.classList.add("missionaries_card");
-
-//       missionaryCard.innerHTML = `
-//           <img src="${missionary.image}" loading="lazy" alt="Profile picture" class="profile">
-//           <div class="details">
-//               <h3>${missionary.name}</h3>
-//               <h3>Departure Date: ${missionary.departureDate}</h3>
-//               <h3>Mission: ${missionary.mission}</h3>
-//               <h3>Mail:</h3>
-//               <a style="display:none;" href="mailto:${missionary.mail}" style="font-size: 0.75rem;">
-//                     <h4>${missionary.mail}</h4>
-//                 </a>
-//                <a style=text-decoration="underline;" ></a>
-//               <button style="display:none;" class="button open-button">More</button>
-//           </div>
-//         `;
-//       missionariesList.appendChild(missionaryCard);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching missionaries:", error);
-//   });
