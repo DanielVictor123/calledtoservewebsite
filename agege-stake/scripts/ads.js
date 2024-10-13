93400200c8d3bd9187798226ae8eae1dee4c408a
DanielVictor123 <isaacvictor942006@gmail.com>
2024-10-13 23:50:28 +0100

    After launch rap-up

--- agege-stake/scripts/ads.js
+++ agege-stake/scripts/ads.js
@@ -1,21 +1,10 @@
-// // Hide the card initially and display it after 5 seconds
-// setTimeout(() => {
-//   document.querySelector(".modal-card").style.display = "block";
-// }, 7000);
 
-// // Hide the card when the exit button is clicked
-// document.querySelector(".exit-btn").addEventListener("click", () => {
-//   document.querySelector("#box-card").style.display = "none";
-// });
-console.log("CLICKED!!!!!!!!!!");
-// Show the modal card after 7 seconds
+// Show the modal card after 15 seconds
 setTimeout(() => {
   document.querySelector("#ads").style.display = "block";
-}, 10000);
+}, 15000);
 
 // Hide the card when the exit button is clicked
 document.querySelector(".exit").addEventListener("click", () => {
-  console.log("CLICKED!!!!!!!!!!");
   document.querySelector("#ads").style.display = "none";
-  // document.querySelector("#box-card").style.display = "none";
 });
