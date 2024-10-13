9fc050cffef1087dbf07dca3d8314c0bf6507a63
DanielVictor123 <isaacvictor942006@gmail.com>
2024-10-13 13:43:20 +0100

    Full Final Update

--- agege-stake/scripts/test-users.js
+++ agege-stake/scripts/test-users.js
@@ -26,9 +26,6 @@ fetch("missionaries.json")
             <h6 class="mb-2 text-muted">Departure Date: ${
               missionary.departureDate
             }</h6>
-            <h6 class="mb-2 text-muted">Return Date: ${
-              missionary.returnDate || "Not Available"
-            }</h6>
             <h6 class="mb-2 text-muted">Mission: ${missionary.mission}</h6>
             <h6 class="mb-2 text-muted">Mail: ${
               missionary.email || "Not Provided"
